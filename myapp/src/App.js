4fcb29320c704a80363c6fb987da155d4a56658e
Manuka Mayurajith <139702673+manuka8@users.noreply.github.com>
2024-11-26 09:04:42 +0000

    Modify App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
+import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
 import './App.css';
-
+import Home from './components/Home';
+import Contactus from './components/Contactus';
+import Aboutus from './components/Aboutus';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+        <Route path='/home' element={<Home/>} />
+        <Route path='/contact' element={<Contactus/>} />
+        <Route path='/about' element={<Aboutus/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
